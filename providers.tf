b275037e9dc1235975a6c96a42d3a5f7e63fe853
Asma Boudjatat <37596053+boudjasma@users.noreply.github.com>
2022-03-03 00:55:07 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,13 +1,4 @@
-provider  "azurerm"  {
-	client_id = var.client_id
-	client_secret = var.client_secret
-	tenant_id = var.tenant_id
-	subscription_id = var.subscription_id
-	features  {}
-}
-provider  "databricks"  {
-	host = azurerm_databricks_workspace.example.workspace_url
-	azure_client_id = var.client_id
-	azure_client_secret = var.client_secret
-	azure_tenant_id = var.tenant_id
+provider  "google"  {
+	project  ="projetcloud-342322"
+	region   ="eu (plusieurs régions dans l'Union européenne)"
 }
